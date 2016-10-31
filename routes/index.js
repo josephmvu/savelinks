52573643544b1bb425a27dd99164ecb8429b3274
Joseph Vu <hello@josephvu.me>
2016-10-31 03:46:04 +0000

    add login and signup views

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,12 @@ router.get('/', (req, res) => {
     res.render('index');
 });
 
+router.get('/signup', (req, res) => {
+    res.render('signup');
+});
+
+router.get('/login', (req, res) => {
+    res.render('login');
+});
+
 module.exports = router;
